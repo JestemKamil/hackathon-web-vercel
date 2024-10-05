cd7c271f8d6a6e256687dad0c4fedff0ef4a000f
JestemKamil <kamilwasik15@gmail.com>
2024-10-05 22:44:53 +0200

    add acceternity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+const {
+    default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          '"Inter"',
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
-      },
+    content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    '"Inter"',
+                    "ui-sans-serif",
+                    "system-ui",
+                    "sans-serif",
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    '"Noto Color Emoji"',
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    darkMode: "class",
+    plugins: [],
+} satisfies Config
+
+function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme("colors"))
+    let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+    )
+
+    addBase({
+        ":root": newVars,
+    })
+}
