f6cc78362c55b66e8afbe1e23e9f9ac9f6b13eca
JestemKamil <kamilwasik15@gmail.com>
2024-10-05 20:20:16 +0200

    add test content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,10 +3,13 @@ import type { MetaFunction } from "@remix-run/node"
 export const meta: MetaFunction = () => {
     return [
         { title: "Hackathon Web" },
-        { name: "description", content: "Web app for NASA Space Apps hackathon " },
+        {
+            name: "description",
+            content: "Web app for NASA Space Apps hackathon ",
+        },
     ]
 }
 
 export default function Index() {
-    return <div></div>
+    return <div>test</div>
 }
